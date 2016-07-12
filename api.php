944a2fbe97bc9db353f066ed2e80fdac4e459c84
Carlos Muñoz <carmunoz@gmail.com>
2016-07-11 21:09:12 -0500

    Added new API for TMDB

--- api.php
+++ api.php
@@ -2,21 +2,26 @@
 
 require_once( __DIR__ . '/vendor/autoload.php' );
 
-require_once( 'tmdb_v3-PHP-API/tmdb-api.php' );
+//require_once( 'tmdb_v3-PHP-API/tmdb-api.php' );
+
 // API Key for https://mysterious-beach-72002.herokuapp.com/
 $apikey = "6ddf2ff10893429464a2ad9230699501";
-$tmdb = new TMDB( $apikey, 'en', true );
+//$tmdb = new TMDB( $apikey, 'en', true );
+$token = new  \Tmdb\ApiToken( $apikey );
+$client = new \Tmdb\Client( $token );
 
 $app = new Silex\Application();
 $app['debug'] = true;
 
-$app->get('/actor/{name}', function($name)  use ($app, $tmdb) {
+$app->get('/actor/{name}', function($name)  use ($app, $client) {
 	//$app['monolog']->addDebug('api');
 	//error_log( 'Actor: ' . $name );
 	//return '<pre>Hello '. $app->escape($name) .'</pre>';
-	$persons = $tmdb->searchPerson( $name );	
+	//$persons = $tmdb->searchPerson( $name );	
 	error_log($name);
-	error_log( print_r( $persons, true ) );
+	$movie = $client->getMoviesApi()->getMovie(550);	
+	error_log( print_r( $movie, true ) );
+	//error_log( print_r( $persons, true ) );
 
 	//$movieList = array( "The Schindler's list" );
 	//return $app->json(array( 'actor' => $name, 'movies' => $movieList ));
