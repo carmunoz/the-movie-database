8e410c863787707d6ace4f5fa22f49aa085842a1
Carlos Munnoz <carmunoz@gmail.com>
2016-07-12 23:35:36 -0400

    - Add html required tags to declare encoding and other settings. - Add html element with ID "app" to support main React.js component.

--- index.php
+++ index.php
@@ -4,12 +4,16 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<title>The movie database</title>
+		<meta name="description" content="Simple application to search actors">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
+	<body>
+		<div id="app"></div>
+	</body>
 <?php
 	include_once('include.js.php');
 ?>
-	<body>
-
-		<h1>This is a test</h1>
-	</body>
 </html>
