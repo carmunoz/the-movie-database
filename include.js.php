a2b8871abe8b79d71e5c6adaa23cdb15b2f9e480
Carlos Munnoz <carmunoz@gmail.com>
2016-07-12 23:38:18 -0400

    Rebuild .js with Webpack

--- include.js.php
+++ include.js.php
@@ -1,3 +1,3 @@
-<script type="text/javascript" src="build/app.js?v=5d9c1e6ed2820dd7337a09c40c0b066eaef9d6e8" charset="UTF-8"></script>
+<script type="text/javascript" src="build/app.js?v=4fab0a1af229782cbf5f2104d1f97547ff30bfb4" charset="UTF-8"></script>
 
 
