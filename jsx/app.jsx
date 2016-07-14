7588dee380acb1299f23c40af712afbf31f1596d
Carlos Munnoz <carmunoz@gmail.com>
2016-07-14 00:12:40 -0400

    Initial implementation of Photo component

--- jsx/app.jsx
+++ jsx/app.jsx
@@ -7,15 +7,33 @@ var axios = require('axios');
 // TODO: show a message when the total result is greater than current list (limited to 20 for performance reasons). 
 // Something like 'there are XXX more results...'
 // this should happen when the user search for "John" or other common name.
+// The proper way is to use or create a pagination component.
+
+// TODO: show a clock indicator when there is an Ajax call in progress.
+
+// TODO: add click handler for actors to show the actor's movie list
+
+Photo = React.createClass({
+	render: function() {
+		// TODO: use CSS instead of fixed layout
+		if( ! this.props.url  ) {
+			return <div>No photo available</div>;
+		}
+		else {
+			return <div>
+				<image src={this.props.url} width="120px" />
+			</div>;
+		}
+	}
+});
 
 Actor = React.createClass({
 	render: function(){
 		// TODO: create component to render movie list for the actor
-		// TODO: create component to render the image profile properly
 
 		return <div className="actorresult">
 			<div>{this.props.actor.name}</div>
-			<div>{this.props.actor.profile_path}</div> 
+			<Photo url={this.props.actor.photo_url}/> 
 		</div>;
 	}
 });
