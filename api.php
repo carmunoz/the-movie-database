85e453528e6988b5b56a3dc02a79df9f66ccc2a2
Carlos Munnoz <carmunoz@gmail.com>
2016-07-13 23:52:46 -0400

    Move the $apikey variable to a separate file

--- api.php
+++ api.php
@@ -1,12 +1,7 @@
 <?php
 
 require_once( __DIR__ . '/vendor/autoload.php' );
-
-// API Key for https://mysterious-beach-72002.herokuapp.com/
-//$apikey = "6ddf2ff10893429464a2ad9230699501";
-
-// API Key for GPS
-$apikey = "4813cfa463e06f405c9829adc7e239e2";
+require_once( 'apikey.php' );
 
 $token = new  \Tmdb\ApiToken( $apikey );
 $client = new \Tmdb\Client( $token );
