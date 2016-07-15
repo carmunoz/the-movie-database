562785b418048bd9218db356ef6162e4b74bd133
Carlos Munnoz <carmunoz@gmail.com>
2016-07-15 00:21:41 -0400

    Add polyfills to support old browsers

--- jsx/app.jsx
+++ jsx/app.jsx
@@ -1,3 +1,7 @@
+require('es5-shim');
+require('es5-shim/es5-sham');
+require('console-polyfill');
+
 var React = require('react');
 var ReactDOM = require( 'react-dom' );
 var axios = require('axios');
