b3decfa94392587bdb0f3060ade9ba93cb72a352
Carlos Munnoz <carmunoz@gmail.com>
2016-07-15 08:22:28 -0400

    Rebuild .js

--- include.js.php
+++ include.js.php
@@ -1,3 +1,3 @@
-<script type="text/javascript" src="build/app.js?v=f14891430c83685cddc7cbb9a5bef2870e81abc3" charset="UTF-8"></script>
+<script type="text/javascript" src="build/app.js?v=386430053e8068ea7e2c2d461ecd2be00e0fd657" charset="UTF-8"></script>
 
 
