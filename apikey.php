592fa8107dc6046dc59e5017d959a749d7be1dfa
Carlos Munnoz <carmunoz@gmail.com>
2016-07-14 23:43:50 -0400

    Update to match demo site

--- apikey.php
+++ apikey.php
@@ -1,7 +1,7 @@
 <?php
 // API Key for https://mysterious-beach-72002.herokuapp.com/
-//$apikey = "6ddf2ff10893429464a2ad9230699501";
+$apikey = "6ddf2ff10893429464a2ad9230699501";
 
 // API Key for GPS
-$apikey = "4813cfa463e06f405c9829adc7e239e2";
+//$apikey = "4813cfa463e06f405c9829adc7e239e2";
 
